4d9fe543192f5668c887e217001e039ebb51db5c
lokhandeganesh <lokhandeganesh8@gmail.com>
2023-04-12 15:26:30 +0530

    added password field with toggel

---
+++ website/js/scripts.js
@@ -0,0 +1,27 @@
+// Toggle password - eye icon visibility
+// const PassBtn = document.querySelector('#passBtn');
+
+// PassBtn.addEventListener('click',()=>{
+//   const input = document.querySelector('#passInput');
+//   input.getAttribute('type') === 'password'?input.setAttribute('type','text'):input.setAttribute('type','password');
+
+//   if (input.getAttribute('type') === 'text'){
+//     PassBtn.innerHTML = '<i class = "fa fa-eye-slash"></i>';
+//   }else{
+//     PassBtn.innerHTML = '<i class = "fa fa-eye fa-fw" aria-hidden="true"></i>';
+//   }
+// });
+
+const PassBtn = document.querySelector("#passBtn");
+PassBtn.addEventListener("click", () => {
+  const input = document.querySelector("#passInput");
+  input.getAttribute("type") === "password"
+    ? input.setAttribute("type", "text")
+    : input.setAttribute("type", "password");
+
+  if (input.getAttribute("type") === "text") {
+    PassBtn.innerHTML = '<i class="fa fa-eye-slash"></i>';
+  } else {
+    PassBtn.innerHTML = '<i class="fa fa-eye fa-fw" aria-hidden="true"></i>';
+  }
+});
