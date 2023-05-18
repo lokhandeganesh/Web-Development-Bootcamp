e7bf447a532eecd1ae4fca5f7b0bbebbfed65020
lokhandeganesh <lokhandeganesh8@gmail.com>
2023-05-18 20:23:42 +0530

    Completed Blog Post Website

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -61,22 +62,16 @@ app.post("/compose", function (req, res) {
 // Reroute("/posts/")
 app.get("/posts/:postName", function (req, res) {
   // console.log(res.send(req.params.postName));
-  let requiredPostName = req.params.postName;
-
-  // function getObject(postName) {
-  //   let obj = posts.find((object) => object.inputBlogTitle === postName);
-  //   return obj;
-  // }
-
-  // if (getObject(requiredPostName)) {
-  //   console.log("Found");
-  // }
+  let reqBlogTitle = _.kebabCase(req.params.postName);
 
   posts.forEach(function (post) {
-    const storedInputBlogTitle = post.inputBlogTitle;
+    const storBlogTitle = _.kebabCase(post.inputBlogTitle);
 
-    if (storedInputBlogTitle === requiredPostName) {
-      console.log("Found");
+    if (storBlogTitle === reqBlogTitle) {
+      res.render("post", {
+        inputBlogTitle: post.inputBlogTitle,
+        inputBlogText: post.inputBlogText,
+      });
     } else {
       console.log("Not Found");
     }
