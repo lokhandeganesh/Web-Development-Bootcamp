9918b27df6c09686ac1a14d20253983902d1eaf9
lokhandeganesh <lokhandeganesh8@gmail.com>
2023-05-23 18:05:19 +0530

    intro to react complet

---
+++ React_APP/first-app/src/componenets/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>for Title</h1>
+      <p>for the content</p>
+    </div>
+  );
+}
+
+export default Note;
---
+++ React_APP/first-app/src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// 
+import App from "./componenets/App";
+
+// 
+// const element = <h1>this is something written in JS</h1>;
+// 
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
