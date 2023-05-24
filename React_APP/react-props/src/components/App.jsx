852c9492694a4d50c919b86b9eaf78f573c530cc
lokhandeganesh <lokhandeganesh8@gmail.com>
2023-05-24 12:26:06 +0530

    mapping data to components

--- React_APP/react-props/src/components/App.jsx
+++ React_APP/react-props/src/components/App.jsx
@@ -3,19 +3,35 @@ import Card from './Card';
 // 
 import Contacts from '../contacts'
 
-const card = [];
-Contacts.forEach(contact => {
-  card.push(<Card key={contact.id} name={contact.name} img_src= {contact.imgURL} phone={contact.phone} email={contact.email} />)
-  });
+// const card = [];
+// Contacts.forEach(contact => {
+//   card.push(<Card key={contact.id} name={contact.name} img_src= {contact.imgURL} phone={contact.phone} email={contact.email} />)
+//   });
+
+function createCard(contact) {
+  return(
+    <Card 
+    key={contact.id} 
+    id = {contact.id}
+    name={contact.name} 
+    img_src= {contact.imgURL} 
+    phone={contact.phone} 
+    email={contact.email}
+    />
+  );
+}
 
 
+// 
 function App() {
   return (
     
     <div> 
       <React.StrictMode>      
       <h1 className="heading">My Contacts</h1>
-      {card}   
+      {/* {card} */}
+      {Contacts.map(createCard)}
+      
       </React.StrictMode>     
     </div>
     
